f0a4b4a13ed9d1f69d3f0b4a49ebc326a870539d
sette7blo <sette7blo@users.noreply.github.com>
2026-07-12 21:18:56 +0000

    make workstr web app only

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import './style.css';
-import { renderLanding, shouldRenderApp } from './app/landing';
 import { renderShell } from './app/shell';
 import { registerServiceWorker } from './app/pwa';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 if (!app) throw new Error('missing app root');
 
-if (shouldRenderApp()) {
-  renderShell(app);
-  registerServiceWorker();
-} else {
-  renderLanding(app);
-}
+renderShell(app);
+registerServiceWorker();
